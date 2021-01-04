e8aaccedebb0a4d13c877f29c2d57a9f4d198a9d
Jacob Fliss <newgmobile@gmail.com>
2021-01-04 14:11:06 -0500

    some Python work, moved Unity folder to CSharp folder

--- python/client.py
+++ python/client.py
@@ -10,42 +10,45 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 
 BUFFER_SIZE = 1024
-MESSAGE = 'Hello, World!'.encode()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 class myThread (threading.Thread):
-    def __init__(self, ip, port):
+    def __init__(self, username, ip, port):
         threading.Thread.__init__(self)
         self.ip = ip
-        self.port = port
+        self.port = int(port)
+        self.username = username
+
+        print('username:', username)
+        print('ip:', ip)
+        print('port:', port)
 
     def run(self):
         # print('ip:', self.ip)
         s.connect((self.ip, self.port))
-        s.send(MESSAGE)
+        s.send(("con|" + self.username).encode())
 
         # listen
         while 1:
             data = s.recv(BUFFER_SIZE)
             if not data:
                 break
-            # print('received data:', data)
+            print('received data:', data)
             Client_UI.self.text_output(text=data)
             time.sleep(5)
 
         s.close()
 
 
-def connectbtn_callback(instance, ip, port):
-    print("connecting...")
-    print('ip:', instance)
-    print('port:', ip)
-    thread1 = myThread(instance, ip)
-    thread1.start()
-
-
 class Client_UI(App):
+    def connectbtn_callback(self, value):
+        print("connecting...")
+        print('ip:', self.input_ip.text)
+        print('port:', self.input_port.text)
+        thread1 = myThread(self.input_username.text, self.input_ip.text, self.input_port.text)
+        thread1.start()
+    
     def build(self):
         root = FloatLayout()
 
@@ -53,29 +56,28 @@ class Client_UI(App):
         layout_top = BoxLayout(orientation='horizontal', size_hint=(1, .1))
         layout_bottom = BoxLayout(orientation='horizontal', size_hint=(1, .1))
 
-        text_input = TextInput()
-        text_output = TextInput()
-        input_username = TextInput(text="user", multiline=False)
-        input_ip = TextInput(text="127.0.0.1", multiline=False)
-        input_port = TextInput(text="5005", multiline=False)
+        self.text_input = TextInput()
+        self.text_output = TextInput()
+        self.input_username = TextInput(text="user", multiline=False)
+        self.input_ip = TextInput(text="127.0.0.1", multiline=False)
+        self.input_port = TextInput(text="5005", multiline=False)
         connect_button = Button(text="CONNECT")
-        connect_button.bind(on_press=partial(
-            connectbtn_callback, input_ip.text, input_port.text))
+        connect_button.bind(on_press=self.connectbtn_callback)
         # connect_button.bind(on_press=connectbtn_callback)
         send_button = Button(text="SEND", size_hint=(.3, 1))
 
         layout_top.add_widget(Label(text='user'))
-        layout_top.add_widget(input_username)
+        layout_top.add_widget(self.input_username)
         layout_top.add_widget(Label(text='ip'))
-        layout_top.add_widget(input_ip)
+        layout_top.add_widget(self.input_ip)
         layout_top.add_widget(Label(text='port'))
-        layout_top.add_widget(input_port)
+        layout_top.add_widget(self.input_port)
         layout_top.add_widget(connect_button)
         layout.add_widget(layout_top)
 
-        layout.add_widget(text_input)
+        layout.add_widget(self.text_input)
 
-        layout_bottom.add_widget(text_output)
+        layout_bottom.add_widget(self.text_output)
         layout_bottom.add_widget(send_button)
         layout.add_widget(layout_bottom)
 
--- python/readme.md
+++ python/readme.md
@@ -1,1 +1,3 @@
-not finished
+The client uses Kivy. You can read how to install this via https://kivy.org/doc/stable/gettingstarted/installation.html
+
+Still incomplete!
