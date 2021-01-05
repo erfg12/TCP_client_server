9779a162d032690fc3aa90f909da701542eab18a
erfg12 <newgmobile@gmail.com>
2021-01-04 20:21:51 -0500

    python client finished, server needs work

--- python/client.py
+++ python/client.py
@@ -9,7 +9,6 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 
-BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
@@ -20,34 +19,36 @@ class myThread (threading.Thread):
         self.port = int(port)
         self.username = username
 
-        print('username:', username)
-        print('ip:', ip)
-        print('port:', port)
-
     def run(self):
-        # print('ip:', self.ip)
         s.connect((self.ip, self.port))
-        s.send(("con|" + self.username).encode())
+        s.send(('con|' + self.username + '\0').encode())
 
-        # listen
         while 1:
-            data = s.recv(BUFFER_SIZE)
+            data = s.recv(1024)
             if not data:
-                break
-            print('received data:', data)
-            Client_UI.self.text_output(text=data)
-            time.sleep(5)
+                continue
+            print('Received', repr(data))
+            StringData = str(data).replace("\\x00\'", '\n')
+            StringData = StringData.replace("b\'", "")
+            Client_UI.text_output.text += StringData
 
         s.close()
 
 
 class Client_UI(App):
+    text_input = TextInput()
+    text_output = TextInput()
+
     def connectbtn_callback(self, value):
         print("connecting...")
-        print('ip:', self.input_ip.text)
-        print('port:', self.input_port.text)
         thread1 = myThread(self.input_username.text, self.input_ip.text, self.input_port.text)
+        thread1.daemon = True
         thread1.start()
+
+    def sendbtn_callback(self, value):
+        print("sending: " + self.text_input.text)
+        s.send((self.text_input.text + '\0').encode())
+        self.text_input.text = ""
     
     def build(self):
         root = FloatLayout()
@@ -56,15 +57,16 @@ class Client_UI(App):
         layout_top = BoxLayout(orientation='horizontal', size_hint=(1, .1))
         layout_bottom = BoxLayout(orientation='horizontal', size_hint=(1, .1))
 
-        self.text_input = TextInput()
-        self.text_output = TextInput()
         self.input_username = TextInput(text="user", multiline=False)
         self.input_ip = TextInput(text="127.0.0.1", multiline=False)
-        self.input_port = TextInput(text="5005", multiline=False)
+        self.input_port = TextInput(text="13000", multiline=False)
         connect_button = Button(text="CONNECT")
         connect_button.bind(on_press=self.connectbtn_callback)
         # connect_button.bind(on_press=connectbtn_callback)
         send_button = Button(text="SEND", size_hint=(.3, 1))
+        send_button.bind(on_press=self.sendbtn_callback)
+
+        self.text_output.readonly = True
 
         layout_top.add_widget(Label(text='user'))
         layout_top.add_widget(self.input_username)
@@ -75,9 +77,9 @@ class Client_UI(App):
         layout_top.add_widget(connect_button)
         layout.add_widget(layout_top)
 
-        layout.add_widget(self.text_input)
+        layout.add_widget(self.text_output)
 
-        layout_bottom.add_widget(self.text_output)
+        layout_bottom.add_widget(self.text_input)
         layout_bottom.add_widget(send_button)
         layout.add_widget(layout_bottom)
 
--- python/server.py
+++ python/server.py
@@ -1,22 +1,43 @@
 import socket
-
+import time
+import threading
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+TCP_PORT = 13000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(50)
 
-conn, addr = s.accept()
-print('Connection address:', addr)
+class myThread(threading.Thread):
+    def __init__(self):
+        print("server has started")
+        threading.Thread.__init__(self)
+        conn, addr = s.accept()
+        
+        print('Connection address:', addr)
+    
+    def Command(self, value):
+        return value
 
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data:
-        break
-    print('received data:', data)
-    conn.send(data)  # echo
+    def run(self):
+        print("Server listening")
+        while 1:
+            data = conn.recv(1024)
+            if not data:
+                continue
+            StringData = str(data).replace("\\x00\'", '\n')
+            StringData = StringData.replace("b\'", "")
+            print('received data:', StringData)
+            if '|' in StringData:
+                Command(StringData)
+            conn.send(data)
+        conn.close()
 
-conn.close()
+class Server():
+    def run(self):
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(50)
+
+        thread1 = myThread()
+        thread1.start()
+
+Server().run()
